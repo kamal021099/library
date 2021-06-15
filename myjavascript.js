9f94b4e64b9c354f8d7862b8fb14d0a89c8629e8
Kamal <kamal021099@gmail.com>
2021-06-15 08:18:38 +0530

    library project from the odin project, has a book object constructor

---
+++ myjavascript.js
@@ -0,0 +1,14 @@
+function Books(title, author, pages, read)
+{
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	
+	this.info = function()
+		{
+			return (title + " by " + author + ", " + pages + " ," + read) ;
+ 
+		}
+
+}
