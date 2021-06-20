d7d86a6fd659af96083cd649288fbd7110174d56
Kamal <kamal021099@gmail.com>
2021-06-20 09:32:12 +0530

    added delete button to all cards

--- myjavascript.js
+++ myjavascript.js
@@ -62,18 +62,19 @@ myLibrary.push(z);
 
 let displayed = 0;  //to not let the books reapeat in the for loop when displayed.
 function displayBooks() {
-	const container = document.querySelector(".container");
+	 let container = document.querySelector(".container");
 	for(displayed; displayed<myLibrary.length; displayed++)
 	{
 
 		let card = document.createElement('div');
 		card.classList.add('card');
+		// card.setAttribute("data-key", displayed); unnecessary
 		card.setAttribute("id",myLibrary[displayed].title);
 		
 		let title = document.createElement("p");
 		title.textContent = "title: " + myLibrary[displayed].title;
 		card.appendChild(title);
-
+		
 		let author = document.createElement("p");
 		author.textContent = "author: " + myLibrary[displayed].author;
 		card.appendChild(author);
@@ -85,8 +86,16 @@ function displayBooks() {
 		let read = document.createElement("p");
 		read.textContent = "read: " + myLibrary[displayed].read;
 		card.appendChild(read);
+		
+		let del = document.createElement("button");
+		del.textContent = "del";
+		del.classList.add("delete");
+		del.setAttribute("data-key", displayed);
+		card.appendChild(del);
 
 		container.appendChild(card);
+
+		delEvent();
 
 	}
 }
@@ -102,6 +111,9 @@ const add = document.querySelector("#add");
 add.addEventListener('click', () => {
 	let form = document.querySelector("form");
 	form.style.display = "block";
+	form.style.zIndex = "1";
+	form.style.width = "100%";
+	form.style.height = "100%";
 }
 )
 
@@ -110,4 +122,31 @@ submit.addEventListener('click', (e) => {
 	e.preventDefault();
 	addBookToLibrary();
 	displayBooks();
+	document.querySelector('form').reset();
+	document.querySelector('form').style.display = "none";
 })
+
+
+function delEvent(){
+	
+	const dels = document.querySelectorAll(".delete");
+	dels.forEach(del => {
+		del.addEventListener('click', delCard);
+	})
+
+}
+
+function delCard(e){
+	let key = e.target.getAttribute("data-key");
+	console.log(key);
+	console.log(myLibrary);
+	
+	myLibrary.splice(key,1);
+	console.log(myLibrary);
+
+	// //always delete the prev container before rerunning such fn.
+	document.querySelector(".container").innerHTML = '';
+	displayed = 0;
+	displayBooks();
+
+}
